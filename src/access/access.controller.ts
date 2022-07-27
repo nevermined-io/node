192d4761e494ad4786ed8fc5d9dc1f83e0bbd60d
Sami Mäkelä <mrsmkl@gmail.com>
2022-07-27 18:31:26 +0300

    nft transfer works, but need some refactoring in sdk

--- src/access/access.controller.ts
+++ src/access/access.controller.ts
@@ -1,11 +1,13 @@
-import { BadRequestException, Controller, Get, Param, Req, Response, StreamableFile } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { BadRequestException, Body, Controller, Get, Param, Post, Req, Response, StreamableFile } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiProperty, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Request } from '../common/helpers/request.interface';
 import { Nevermined } from '@nevermined-io/nevermined-sdk-js'
 import { config } from '../config'
 import { decrypt } from "../common/helpers/utils";
 import download from 'download';
-
+import { IsNumber, IsString } from "class-validator";
+import { Public } from "../common/decorators/auth.decorator";
+import { validateAgreement } from '../common/helpers/agreement';
 export class AccessResult {
   res: string
 }
@@ -31,6 +33,36 @@ async function downloadAsset(did: string, index: number, res: any): Promise<Stre
     return new StreamableFile(contents)
   }
   throw new BadRequestException()
+}
+
+export class TransferDto {
+  @ApiProperty({
+    description: 'The agreement for NFT transfer',
+    example: '0x...'
+  })
+  @IsString()
+  agreementId: string;
+
+  @ApiProperty({
+    description: 'NFT holder address',
+    example: '0x...'
+  })
+  @IsString()
+  nftHolder: string;
+
+  @ApiProperty({
+    description: 'NFT receiver address',
+    example: '0x...'
+  })
+  @IsString()
+  nftReceiver: string;
+
+  @ApiProperty({
+    description: 'Number of NFTs to transfer',
+    example: '1'
+  })
+  @IsNumber()
+  nftAmount: number;
 }
 
 @ApiTags('Access')
@@ -72,6 +104,38 @@ export class AccessController {
     @Param('index') index: number,
   ): Promise<StreamableFile> {
     return await downloadAsset(req.user.did, index, res)
+  }
+
+  @Post('nft-transfer')
+  @ApiOperation({
+    description: 'Access asset',
+    summary: 'Public',
+  })
+  @Public()
+  @ApiResponse({
+    status: 200,
+    description: 'Return the url of asset',
+  })
+  async doNftTransfer(@Body() transferData: TransferDto): Promise<string> {
+    console.log('going to transfer', transferData)
+    const nevermined = await Nevermined.getInstance(config)
+    const params = nevermined.keeper.templates.nftSalesTemplate.params(transferData.nftReceiver, transferData.nftAmount, transferData.nftHolder)
+    const conditions = [
+      {name: 'lock', fulfill: false},
+      {name: 'transfer', fulfill: true, delegate: true, condition: nevermined.keeper.conditions.transferNftCondition},
+      {name: 'escrow', fulfill: true, condition: nevermined.keeper.conditions.escrowPaymentCondition},
+    ]
+    const agreement_id = transferData.agreementId
+    const agreement = await nevermined.keeper.agreementStoreManager.getAgreement(agreement_id)
+    await validateAgreement({
+      agreement_id,
+      did: agreement.did,
+      params,
+      template: nevermined.keeper.templates.nftSalesTemplate,
+      conditions,
+    })
+    console.log('fulfilled agreement')
+    return 'success'
   }
 
   @Get('download/:index')
--- src/common/helpers/agreement.ts
+++ src/common/helpers/agreement.ts
@@ -10,12 +10,14 @@ export interface Template<T> {
 
 export interface NormalCondition {
   fulfillInstance: (a: ConditionInstance<{}>, b: {}, from: Account) => Promise<any>
+  sendFrom: (name: string, args: any[], from: Account) => Promise<any>
 }
 
 export interface ConditionInfo {
   fulfill: boolean,
   condition?: NormalCondition,
   name: string,
+  delegate?: boolean,
 }
 
 export interface Params<T> {
@@ -35,10 +37,12 @@ export async function validateAgreement<T>({
 }: Params<T>) {
   const nevermined = await Nevermined.getInstance(config)
   const ddo = await nevermined.assets.resolve(did)
+  /*
   const templateId: string = await nevermined.keeper.agreementStoreManager.call('getAgreementTemplate', [
     agreement_id
   ])
   console.log('template', templateId, 'for', agreement_id)
+  */
   const agreement = await nevermined.keeper.agreementStoreManager.getAgreement(agreement_id)
   const agreementData = await template.instanceFromDDO(
     agreement.agreementIdSeed,
@@ -60,8 +64,17 @@ export async function validateAgreement<T>({
     }
   }))
   for (let {idx, a} of conditions.map((a,idx) => ({idx, a}))) {
-    if (a.fulfill) {
-      // console.log('fulfilling', a, idx)
+    if (a.fulfill && a.delegate) {
+      const condInstance = agreementData.instances[idx] as ConditionInstance<{}>
+      const [z,b,c,d,_e,f] = await condInstance.params({})
+      console.log('got params', [z,b,c,d,_e,f])
+      await a.condition.sendFrom('fulfillForDelegate', [agreement_id, z, (params as any).providerId, b,c,d,f], from)
+      const lock_state = await nevermined.keeper.conditionStoreManager.getCondition(agreementData.instances[idx].id)
+      if (lock_state.state !== ConditionState.Fulfilled) {
+        throw new UnauthorizedException(`In agreement ${agreement_id}, ${a.name} condition ${agreementData.instances[idx].id} is not fulfilled`)
+      }
+    } else if (a.fulfill) {
+        // console.log('fulfilling', a, idx)
       const condInstance = agreementData.instances[idx] as ConditionInstance<{}>
       await a.condition.fulfillInstance(condInstance, {}, from)
       const lock_state = await nevermined.keeper.conditionStoreManager.getCondition(agreementData.instances[idx].id)
