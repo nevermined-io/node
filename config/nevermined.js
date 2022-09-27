1e8eb42f4c5ae739ffc6103c29b241ab26e576ed
Sami Mäkelä <mrsmkl@gmail.com>
2022-09-27 15:57:29 +0300

    unit test working

--- config/nevermined.js
+++ config/nevermined.js
@@ -6,71 +6,17 @@ const nograph = process.env['NO_GRAPH'] === 'true'
 
 const configBase = {
     nodeUri: process.env['KEEPER_URL'] || 'http://localhost:8545',
-    marketplaceUri: 'http://nevermined-metadata:3100',
-    faucetUri: 'http://localhost:3001',
-    gatewayUri: 'http://localhost:8030',
-    secretStoreUri: 'http://localhost:12001',
-    gatewayAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0',
+    marketplaceUri: process.env['MARKETPLACE_URI'] || 'http://nevermined-metadata:3100',
+    faucetUri: process.env['FAUCET_URI'] || 'http://localhost:3001',
+    gatewayUri: process.env['GATEWAY_URI'] || 'http://localhost:8030',
+    gatewayAddress: process.env['GATEWAY_ADDRESS'] || '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0',
     artifactsFolder: './artifacts',
-    marketplaceAuthToken: 'bogus',
+    marketplaceAuthToken: process.env['MARKETPLACE_AUTH_TOKEN'] || 'bogus',
     graphHttpUri: nograph
         ? undefined
-        : 'http://localhost:9000/subgraphs/name/nevermined-io/development',
+        : process.env['GRAPH_HTTP_URI'] || 'http://localhost:9000/subgraphs/name/nevermined-io/development',
     gasMultiplier: 1.1,
     verbose: LogLevel.Error
-}
-
-if (process.env.NETWORK_NAME === 'production') {
-    Object.assign(configBase, {
-        nodeUri: 'http://localhost:8545',
-        marketplaceUri: 'http://nevermined-metadata:3100',
-        faucetUri: 'http://localhost:3001',
-        gatewayUri: 'http://localhost:8030',
-        secretStoreUri: 'http://localhost:12001',
-        gatewayAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0'
-    })
-}
-
-if (process.env.NETWORK_NAME === 'integration') {
-    Object.assign(configBase, {
-        nodeUri: 'http://localhost:8545',
-        marketplaceUri: 'http://nevermined-metadata:3100',
-        faucetUri: 'http://localhost:3001',
-        gatewayUri: 'http://localhost:8030',
-        secretStoreUri: 'http://localhost:12001',
-        gatewayAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0'
-    })
-}
-
-if (process.env.NETWORK_NAME === 'testing') {
-    Object.assign(configBase, {
-        nodeUri: 'http://localhost:8545',
-        marketplaceUri: 'http://nevermined-metadata:3100',
-        faucetUri: 'http://localhost:3001',
-        gatewayUri: 'http://localhost:8030',
-        secretStoreUri: 'http://localhost:12001',
-        gatewayAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0'
-    })
-}
-
-if (process.env.NETWORK_NAME === 'rinkeby') {
-    Object.assign(configBase, {
-        marketplaceUri: 'https://metadata.rinkeby.nevermined.rocks',
-        faucetUri: 'https://faucet.rinkeby.nevermined.rocks',
-        gatewayUri: 'https://gateway.rinkeby.nevermined.rocks',
-        nodeUri: `https://rinkeby.infura.io/v3/52b6d403f7de4757ab9ed23c3778a35b`,
-        gatewayAddress: '0x068Ed00cF0441e4829D9784fCBe7b9e26D4BD8d0'
-    })
-}
-
-if (process.env.NETWORK_NAME === 'mumbai') {
-    Object.assign(configBase, {
-        marketplaceUri: 'https://metadata.mumbai.nevermined.rocks',
-        faucetUri: 'https://faucet.mumbai.nevermined.rocks',
-        gatewayUri: 'https://gateway.mumbai.nevermined.rocks',
-        nodeUri: `https://rpc-mumbai.maticvigil.com/v1/e145ac0424e2a2b3c340685c80a08e77099ce020`,
-        gatewayAddress: '0x068Ed00cF0441e4829D9784fCBe7b9e26D4BD8d0'
-    })
 }
 
 configBase.accounts = []
