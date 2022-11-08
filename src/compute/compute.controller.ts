396d3d43775617e1926ce8d5c8ffc21428d08845
josepablofm78 <jp@keyko.io>
2022-11-08 13:27:09 +0100

    sending workflow to argo

--- src/compute/compute.controller.ts
+++ src/compute/compute.controller.ts
@@ -143,16 +143,11 @@ import {
 
        try {
 
-            // TODO. Replace for NVM Compute workflow
-            // let argoWorkflow = this.computeService.readExample()
             const argoWorkflow = await this.computeService.createArgoWorkflow(initData);
-            return JSON.stringify(argoWorkflow);
 
-            /*
             const response = await this.argoWorkflowApi.workflowServiceCreateWorkflow( { serverDryRun:false, namespace: this.argoNamespace, workflow: argoWorkflow}, this.argoNamespace)
             Logger.debug("Argo Workflow created:: " + JSON.stringify(response.data))
-            return response.data.metadata.name
-            */
+            return response.data.metadata.name   
 
         }catch(e) {
             Logger.error(`Problem initialing workflow for service Agreement ${initData.agreementId}. Error: ${e}`);
