90b7db5570e8f5ae3042f3624b074f51a6bbe304
josepablofm78 <jp@keyko.io>
2022-11-18 10:27:22 +0100

    changing route for compute endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -14,4 +14,4 @@ export const routes: Routes = [
   { path: '/', module: InfoModule },
 ];
 
-if (exposeCompute) routes.push({ path: '/api/v1/gateway/compute', module: ComputeModule })
+if (exposeCompute) routes.push({ path: '/api/v1/node/compute', module: ComputeModule })
