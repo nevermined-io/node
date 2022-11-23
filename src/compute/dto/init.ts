7a1e5e3ec3b4ff73920b861f8b781a2e0720b0ab
josepablofm78 <jp@keyko.io>
2022-11-23 08:50:41 +0100

    removing agreement id from body of post request

--- src/compute/dto/init.ts
+++ src/compute/dto/init.ts
@@ -2,12 +2,6 @@ import { ApiProperty } from "@nestjs/swagger";
 import {  IsString } from "class-validator";
 
 export class InitDto {
-    @ApiProperty({
-      description: 'The agreement for Compute Workflow DDO',
-      example: '0x...'
-    })
-    @IsString()
-    agreementId: string;
 
     @ApiProperty({
       description: 'The Did for the Compute Workflow DDO',
