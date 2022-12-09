74b44b7ab220a5f47cec30adae6b77d102c31d58
josepablofm78 <jp@keyko.io>
2022-12-09 09:34:34 +0100

    fix semicolon issue

--- scripts/getdoc.ts
+++ scripts/getdoc.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ApplicationModule } from '../src/app.module';
-import * as path from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { writeFileSync } from 'fs';
-import { readFileSync } from 'fs';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ApplicationModule } from '../src/app.module'
+import * as path from 'path'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { writeFileSync } from 'fs'
+import { readFileSync } from 'fs'
 
 const bootstrap = async () => {
-  const app = await NestFactory.create<NestExpressApplication>(ApplicationModule, { cors: true });
-  app.enable('trust proxy');
+  const app = await NestFactory.create<NestExpressApplication>(ApplicationModule, { cors: true })
+  app.enable('trust proxy')
 
-  const packageJsonPath = path.join(__dirname, '..', 'package.json');
-  const packageJsonString = readFileSync(packageJsonPath, 'utf8');
-  const packageJson = JSON.parse(packageJsonString) as { version: string };
+  const packageJsonPath = path.join(__dirname, '..', 'package.json')
+  const packageJsonString = readFileSync(packageJsonPath, 'utf8')
+  const packageJson = JSON.parse(packageJsonString) as { version: string }
 
   const options = new DocumentBuilder()
     .setTitle('Nevermined Node')
@@ -23,11 +23,11 @@ const bootstrap = async () => {
       },
       'Authorization',
     )
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  writeFileSync('docs/openapi.json', JSON.stringify(document), { encoding: 'utf8' });
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  writeFileSync('docs/openapi.json', JSON.stringify(document), { encoding: 'utf8' })
 
-  await app.close();
+  await app.close()
 };
 
-bootstrap();
+bootstrap()
