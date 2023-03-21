889cb4226c6f224acedd4cc7fb5f19dc0cba49fe
Rodolphe Marques <marques.rodolphe@gmail.com>
2023-03-21 15:58:36 +0100

    feat: increase test timeout for jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   verbose: true,
-  testTimeout: 30000,
+  testTimeout: 300000,
   moduleFileExtensions: ['js', 'json', 'ts'],
   setupFiles: ['jest-date-mock'],
   roots: ['src', 'integration'],
