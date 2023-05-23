ac0f29e2c1c69fb61cf25b1a00a731097983dc54
Sami Mäkelä <mrsmkl@gmail.com>
2023-05-23 15:54:48 +0300

    chore: try updating nvm tools

--- integration/subscriptions.integration.spec.ts
+++ integration/subscriptions.integration.spec.ts
@@ -167,7 +167,7 @@ describe('SubscriptionsController', () => {
     })
   })
 
-  describe('GET /subscriptions should validate the subscription', () => {
+  describe.only('GET /subscriptions should validate the subscription', () => {
     let ddoWebService: DDO
     let ddoSubscription: DDO
     let notSubscriberToken: string
