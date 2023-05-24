c42bb5a7482d18de4c11c0f6e5fd4e9e6ef7e8ac
Rodolphe Marques <marques.rodolphe@gmail.com>
2023-05-24 15:11:19 +0200

    fix: integration tests should use a local version of node

--- integration/config.ts
+++ integration/config.ts
@@ -3,7 +3,7 @@ import { makeAccounts, NeverminedOptions } from '@nevermined-io/sdk'
 const configBase: NeverminedOptions = {
   web3ProviderUri: 'http://contracts.nevermined.localnet',
   marketplaceUri: 'http://marketplace.nevermined.localnet',
-  neverminedNodeUri: 'http://node.nevermined.localnet',
+  neverminedNodeUri: 'http://localhost:8030',
   neverminedNodeAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0',
   marketplaceAuthToken: undefined,
   artifactsFolder: './artifacts',
