461c53496f1736a6353a19436dcd05b0551dcccf
Aitor <1726644+aaitor@users.noreply.github.com>
2023-10-17 08:50:19 +0200

    test: adding subscriptions 1155 integration tests

--- integration/utils.ts
+++ integration/utils.ts
@@ -1,9 +1,10 @@
 import { MetaData } from '@nevermined-io/sdk'
 
-export const getMetadata = (): MetaData => {
+export const getMetadata = (nonce: string | number = Math.random(), name = 'test'): MetaData => {
   return {
     main: {
-      name: 'test',
+      name,
+      ...({ nonce } as any),
       type: 'dataset',
       dateCreated: new Date().toISOString(),
       datePublished: new Date().toISOString(),
